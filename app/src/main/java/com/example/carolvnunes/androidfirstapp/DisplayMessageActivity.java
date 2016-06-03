ceb39f90e070f80d00fcf39cc29d1b2afbde0506
Caroline Visoto Nunes <carolvisoto@gmail.com>
2016-06-04 00:10:55 +0200

    fiz  main to go to second activity

--- app/src/main/java/com/example/carolvnunes/androidfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/carolvnunes/androidfirstapp/DisplayMessageActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -17,9 +16,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/carolvnunes/androidfirstapp/MyActivity.java
+++ app/src/main/java/com/example/carolvnunes/androidfirstapp/MyActivity.java
@@ -2,10 +2,7 @@ package com.example.carolvnunes.androidfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,8 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 
 class MyActivity extends AppCompatActivity {
-//    public final static String EXTRA_MESSAGE = "com.example.carolvnunes.androidfirstapp.MESSAGE";
-    public final static String EXTRA_MESSAGE = "test";
+    public final static String EXTRA_MESSAGE = "com.example.carolvnunes.androidfirstapp.MESSAGE";
     private static final String TAG = MyActivity.class.getSimpleName();
 
     @Override
@@ -22,17 +18,6 @@ class MyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, ">>>>>>>>>>>>>>>>MyActivity onCreate() ");
         setContentView(R.layout.activity_my);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
